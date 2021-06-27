5c278a5e8d05e9d7af9479cfaba509c90e0449ff
Pablo Tedesco <tedesco8@hotmail.com>
2021-06-27 19:37:26 -0300

    se agrega TODO con referencia a doc de coingecko

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View, FlatList } from 'react-native';
 function App() {
   const [coins, setCoins] = useState([])
   const loadData = async () => {
+    //https://www.coingecko.com/es/api#explore-api
     const res = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
     const data = await res.json()
     setCoins(data)
