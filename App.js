d87e120f46f44a66b44a506e29c2e17c50c72593
Pablo Tedesco <tedesco8@hotmail.com>
2021-06-27 19:35:34 -0300

    listando monedas desde coingecko

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-export default function App() {
+function App() {
+  const [coins, setCoins] = useState([])
+  const loadData = async () => {
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+    const data = await res.json()
+    setCoins(data)
+  }
+
+  useEffect(() => {
+    console.log("loaded")
+    loadData()
+  }, [])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={ styles.container }>
+      <Text>Hola desde React Native</Text>
+      <FlatList
+        data={coins}
+        renderItem={({ item }) => {
+          console.log(item.name)
+          return <Text>{item.name}</Text>
+        }}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#141414',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    flex: 1
+  }
+})
+
+export default App
