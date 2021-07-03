35ae975eb05b5c7fdfa5ca3dcacda36e99271345
Pablo Tedesco <pablo.tedesco@vusecurity.com>
2021-07-03 17:36:13 -0300

    se obtiene precio actual, variacion y se le aplican estilos

--- components/CoinItem.js
+++ components/CoinItem.js
@@ -6,9 +6,24 @@ const CoinItem = ({ coin }) => {
     <View style={styles.containerItem}>
       <View style={styles.coinName}>
         <Image style={styles.image} source={{ uri: coin.image }} />
-        <Text style={styles.text}>{coin.name}</Text>
+        <View style={styles.containerNames}>
+          <Text style={styles.text}>{coin.name}</Text>
+          <Text style={styles.textSymbol}>{coin.symbol}</Text>
+        </View>
       </View>
-      <Text style={styles.text}>{coin.name}</Text>
+      <View>
+        <Text style={styles.textPrice}>$ {coin.current_price}</Text>
+        <Text
+          style={[
+            styles.pricePercentage,
+            coin.price_change_percentage_24h > 0
+              ? styles.priceUp
+              : styles.priceDown,
+          ]}
+        >
+          {coin.price_change_percentage_24h}
+        </Text>
+      </View>
     </View>
   );
 };
@@ -20,15 +35,35 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     justifyContent: "space-between",
   },
+  containerNames: {
+    marginLeft: 10,
+  },
   coinName: {
     flexDirection: "row",
-  },
-  text: {
-    color: "#ffffff",
   },
   image: {
     width: 30,
     height: 30,
+  },
+  text: {
+    color: "#ffffff",
+  },
+  textPrice: {
+    textAlign: "right",
+    color: "#ffffff",
+  },
+  textSymbol: {
+    color: "#434343",
+    textTransform: "uppercase",
+  },
+  pricePercentage: {
+    textAlign: "right",
+  },
+  priceUp: {
+    color: "#00B5B9",
+  },
+  priceDown: {
+    color: "#FC4222",
   },
 });
 export default CoinItem;
